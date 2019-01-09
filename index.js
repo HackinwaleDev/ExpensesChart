b25e3c6069444d4f38657779d838714b3e4f9b95
hackinwaleDev <hackinwale.developer@gmail.com>
2019-01-09 01:07:28 +0100

    This project is now completed for maybe final review

--- index.js
+++ index.js
@@ -43,11 +43,13 @@ function drawExpensesChart(){
                 }
             });
             var dailyBudget = data['daily_budget']
-            logger(monthlyExpenses)
+
+            var mySelection = d3.select(this)
+            logger(selection)
             // Select and format the svg
-            var svg = d3.select(this)
-                            .attr('width', svgWidth)
+            var svg = mySelection.attr('width', svgWidth)
                             .attr('height', svgHeight)
+            
             // Place a master container in svg and format
             var chart = svg.append('g')
                             .attr('transform', 'translate('+ margin.left +',' +margin.top+')')
@@ -56,6 +58,10 @@ function drawExpensesChart(){
                 chart.append('text')
                             .attr('x', margin.left)
                             .text('Expenses made this month')
+            // Define custom tooltip
+            var tooltip = d3.select('#svg-container')
+                          .append('div')
+                            .attr('class', 'tooltip')
             //setting the range and scale
             var x = d3.scaleTime()
                             .domain(d3.extent(monthlyExpenses, function(d){ return d.date}))
@@ -97,25 +103,30 @@ function drawExpensesChart(){
                 areaColor.enter().append('stop')
                             .attr('offset', function(d){ return d.offset})
                             .attr('stop-color', function(d){ return d.color})
-            //define the line tool first
+
+            // Define the line tool first
             var line = d3.line()
                             .curve(d3.curveMonotoneX)
                             .x(function(d){ return x(d.date)})
                             .y(function(d){ return y(d.budget)})
-            //Now define the path
+            // Now define the path
             var linePath = chart.append('path')
                             .attr('class', 'line-path')
-                          .datum(monthlyExpenses)
+                          .datum(monthlyExpenses)  
+                            // .on('mouseover', function(d){
+                            //     dotEnter.attr('r', 4)
+                            // }) 
+                            // .on('mouseout', function(d){
+                            //     dotEnter.attr('r', 0)
+                            // })                        
                             .attr('d', line)
-            // implement Tooltip for when moveover
-            
             // Let's define the area too
             var area = d3.area()
                             .curve(d3.curveMonotoneX)
                             .x(function(d){ return x(d.date)})
                             .y0(height)
                             .y1(function(d){ return y(d.budget)})
-            // set path for the area
+            // Set path for the area
             var areaPath = chart.append('path')
                             .attr('class', 'area-path')
                           .datum(monthlyExpenses)
@@ -132,17 +143,57 @@ function drawExpensesChart(){
                             .attr('cx', function(d){ return x(d.date)})
                             .attr('cy', function(d){ return y(d.budget)})
                             .attr('r', 4)
+                            // .attr('visibility', 'hidden')
                             .on('mouseover', function(d,i){
                                 /* Do some magik here */
                                 d3.select(this)
-                                    .select('.dot')
-                                    .style('fill', 'blue')
+                                    .attr('r', 4)
+                                    // .attr('visibility', 'visible')
+                                    .style('stroke', 'green')
+                                    .style('fill', 'white')
+                                    showToolTip(d)
+                            })
+                            .on('mousemove', moveToolTip)
+                            .on('mouseout', function(d){
+                                d3.select(this)
+                                    .attr('r', 4)
+                                    .style('stroke', 'aqua')
+                                    hideToolTip()
                             })
                             .attr('fill', 'white')
-                            .style('stroke', 'green')
+                            .style('stroke', 'aqua')
                             .style('stroke-width', 2)
                             
-
+            // Create custom tooltip 
+            function showToolTip(d) {
+                logger('== Tooltip should show now ==')
+                const formatDay = d3.timeFormat('%a')
+                const formatDate = d3.timeFormat('%B %d')
+                const html = `
+                    <div><span class="header">Date: </span> ${formatDate(d.date)}</div>
+                    <div><span class="header">Budget: </span>$${d.budget}</div>
+                    <div><span class="header">Day: </span> ${formatDay(d.date)}</div>
+                `;
+                tooltip.html(html);
+                // tooltip.text('We are here')
+                tooltip.box = tooltip.node().getBoundingClientRect();
+                tooltip.transition().style("opacity", 1);
+            
+            } // end showToolTip
+            //set coordinates for when tooltip move
+            function moveToolTip() {
+                const top = d3.event.clientY - tooltip.box.height - 5;
+                let left = d3.event.clientX - tooltip.box.width / 2;
+                if (left < 0) {
+                  left = 0;
+                } else if (left + tooltip.box.width > window.innerWidth) {
+                  left = window.innerWidth - tooltip.box.width;
+                }
+                tooltip.style("left", left + "px").style("top", top + "px");
+              }
+            function hideToolTip() {
+                tooltip.transition().style("opacity", 0);
+              }
         }); //end selection
 
     }// end drawChart
